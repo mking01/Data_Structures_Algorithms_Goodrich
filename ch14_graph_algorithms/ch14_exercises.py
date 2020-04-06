4f685d887651cd8dec40252755a7e80c975d656e
mking01 <59142310+mking01@users.noreply.github.com>
2020-04-06 15:41:32 -0400

    Create ch14_exercises.py

---
+++ ch14_graph_algorithms/ch14_exercises.py
@@ -0,0 +1,16 @@
+# Ch. 14 - Graphs
+# p. 686
+
+# 1. Drawn on paper
+
+# Draw a simple undirected graph G that has 12 vertices, 18 edges, and 3 connected components
+
+# 2. If G is a simple undirected graph with 12 vertices and 3 connected components, what is the largest number of edges it might have?
+
+# 9
+
+# 3. Drawn on paper
+
+# 4. Drawn on paper
+
+# 5. On paper
